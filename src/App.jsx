39168e00d8a818889cc2d705c479eea9d5bc6901
John <jbhumph@gmail.com>
2025-04-23 21:14:46 -0700

    Changed to wizards

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './Header.jsx'
 import Card from './Card.jsx'
-import j3 from './assets/j3.jpg'
+import wizard from './assets/wizard.jpg'
 import axios from 'axios'
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
   useEffect(() => {
     const fetchGifs = async () => {
       try {
-        const response = await axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=10`);
+        const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=wizard&limit=10`);
         const gifUrls = response.data.data.map(gif => gif.images.original.url);
         setCards(prevCards =>
           prevCards.map((card, index) => ({
@@ -31,16 +31,16 @@ function App() {
   }, []); 
   
   const [cards, setCards] = useState([
-    { id: 1, name: 'Card 1', image: j3, clicked: false },
-    { id: 2, name: 'Card 2', image: j3, clicked: false },
-    { id: 3, name: 'Card 3', image: j3, clicked: false },
-    { id: 4, name: 'Card 4', image: j3, clicked: false },
-    { id: 5, name: 'Card 5', image: j3, clicked: false },
-    { id: 6, name: 'Card 6', image: j3, clicked: false },
-    { id: 7, name: 'Card 7', image: j3, clicked: false },
-    { id: 8, name: 'Card 8', image: j3, clicked: false },
-    { id: 9, name: 'Card 9', image: j3, clicked: false },
-    { id: 10, name: 'Card 10', image: j3, clicked: false }
+    { id: 1, name: 'Card 1', image: wizard, clicked: false },
+    { id: 2, name: 'Card 2', image: wizard, clicked: false },
+    { id: 3, name: 'Card 3', image: wizard, clicked: false },
+    { id: 4, name: 'Card 4', image: wizard, clicked: false },
+    { id: 5, name: 'Card 5', image: wizard, clicked: false },
+    { id: 6, name: 'Card 6', image: wizard, clicked: false },
+    { id: 7, name: 'Card 7', image: wizard, clicked: false },
+    { id: 8, name: 'Card 8', image: wizard, clicked: false },
+    { id: 9, name: 'Card 9', image: wizard, clicked: false },
+    { id: 10, name: 'Card 10', image: wizard, clicked: false }
   ]);
 
   const shuffleCards = () => {
@@ -69,8 +69,9 @@ function App() {
       <div className="page">
         <div className="header">
           <div className='hL'>
-            <h1>Memory Cards</h1>
-            <div className='subtitle'>A memory card game for The Odin Project</div>
+            <h1>Memory Card Wizard</h1>
+            <div className='subtitle'>A memory card game - click a wizard but never click the same one twice. They're a tricky sort...</div>
+            <div className='subtitle'>Click a card to start!</div>
           </div>
           <div className='hR'>
             <Header count={count} clicks={clicks} />
